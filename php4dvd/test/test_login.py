6dda832a41d7a9e05f2a477b8db1e8c0f7d0caa7
eabramovich <ewg.abramovich2014@yandex.ru>
2016-03-20 15:11:04 +0300

    moving tests to folder test

--- php4dvd/test/test_login.py
+++ php4dvd/test/test_login.py
@@ -9,7 +9,7 @@ import unittest, time, re
 
 class Untitled(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.base_url = "http://localhost/"
         self.verificationErrors = []
